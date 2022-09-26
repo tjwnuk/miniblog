5764f6a2ef1cda4f44a26af4379f44b828bc45bc
tjwnuk <tomasz.wnuk1@gmail.com>
2022-09-26 19:24:02 +0200

    Initial commit

---
+++ src/main/java/io/github/tjwnuk/miniblog/MiniblogApplication.java
@@ -0,0 +1,13 @@
+package io.github.tjwnuk.miniblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tjwnuk/miniblog/MiniblogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tjwnuk.miniblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

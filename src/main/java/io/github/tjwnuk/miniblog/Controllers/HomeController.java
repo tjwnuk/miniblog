8238b1dbe68ac59d93297a5a317ad93b696cbee8
tjwnuk <tomasz.wnuk1@gmail.com>
2022-09-30 17:13:14 +0200

    simple home view

--- src/main/java/io/github/tjwnuk/miniblog/Controllers/HomeController.java
+++ src/main/java/io/github/tjwnuk/miniblog/Controllers/HomeController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @Controller
@@ -16,8 +19,14 @@ public class HomeController {
 
     @GetMapping("")
     public String home(Model model) {
-        Entry wpis = new Entry("admin", "witajcie na naszej stronie", new Date(System.currentTimeMillis()));
-        model.addAttribute("entry", wpis);
+
+        Entry entry1 = new Entry("admin", "witajcie na naszej stronie", new Date(System.currentTimeMillis()));
+        Entry entry2 = new Entry("m__b", "wykop jest do dupy", new Date(System.currentTimeMillis()));
+        Entry entry3 = new Entry("a__s", "Postujcie ciepłe placki", new Date(System.currentTimeMillis()));
+
+        List<Entry> entryList = Arrays.asList(entry1, entry2, entry3);
+
+        model.addAttribute("entryList", entryList);
         return "home";
     }
 }
